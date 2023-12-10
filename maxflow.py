8c19949f4ae34095eb6c64942029ec97e6a0f454
Zhengyu Zou <113187557+zzou9@users.noreply.github.com>
2023-12-09 19:06:49 -0500

    notes for RST14

--- maxflow.py
+++ maxflow.py
@@ -24,12 +24,51 @@ def UltraSparsifyAndReduce(G, kappa):
     return
 
 def CongestionApproximator(G_prime):
+    """
+    From hierarchial decomposition of RST14
+
+    Methods needed: 
+    -   HierarchialDecomp(G): taking in a graph G, return a hierarchial decomposition tree T that has 
+        height log(n) and satisfies RST14 guarantees.
+        -   PartitionA(S): compute a partition of a cluster S of G into z many subclusters Z_1, ..., Z_z
+            such that each subcluster is bounded in size and the set of cutting edges F induces an 1/log^2(n)
+            flow
+            // This follows from Th'm 3.1
+            // Lemma 3.1, Lemma 3.2 implementation
+            -   FindSTEdges(A): given an edge set, compute source set, target set, and separation value \eta 
+                that 
+    -   
+    """
+
+    def HierarchialDecomp(G):
+        """
+        Return a tree data structure
+        See RST14 Section 2
+        """
+
+        def PartitionA(S):
+            """
+            Th'm 3.1, Lemma 3.1, Lemma 3.2
+            """
+        
+            def FindSTEdges(A):
+                A_s, A_t, eta = None, None, None
+                return A_s, A_t, eta
+                
+            
+            return
+
+        return 
+
     return
 
 def Convert(G, G_prime, RG_prime):
     return
 
 def ApproximatorMaxFlow(G, RG, epsilon):
+    """
+    A recursive algo that updates softmax of a potential function given in She13
+    """
     return
 
 
